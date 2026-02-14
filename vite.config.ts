601620aec6f34de9b23b2b50b28662c8d2da07ad
Khamisi Lawrence <khamisilawrencejm@gmail.com>
2026-02-13 20:33:05 -0500

    chore: remove config for external URLs

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/academy": {
-        target: "https://next-gen-academy.genr83d.com/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/academy/, ""),
-      },
-    },
-  },
   plugins: [
     react({
       babel: {
